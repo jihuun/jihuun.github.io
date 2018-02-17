ad47b0d383f750cef17f22a731591b58aa65fb04
Ji-Hun Kim <jihuun.k@gmail.com>
2018-02-17 22:53:51 +0900

    add travel posting 170510
    Signed-off-by: Ji-Hun Kim <jihuun.k@gmail.com>

--- _scripts/make_post.sh
+++ _scripts/make_post.sh
@@ -5,15 +5,18 @@ SAMPLE=_drafts/sample_post.md
 POST=_posts
 FILE=$ROOT_DIR/$POST/2017-$1-$2-#$3.md
 
+echo File name is $FILE
+
 if [[ -e $FILE ]]; then
+	echo "File already exist!"
+else
 	echo "Generating file $FILE"
 	cp $ROOT_DIR/$SAMPLE $FILE
-else
-	echo "File already exist!"
+
+	sed -i "s/MM/$1/g" $FILE
+	sed -i "s/DD/$2/g" $FILE
+	sed -i "s/AA/$3/g" $FILE
+	vi $FILE
 fi
 
-sed -i "s/MM/$1/g" $FILE
-sed -i "s/DD/$2/g" $FILE
-sed -i "s/AA/$3/g" $FILE
-vi $FILE
 
